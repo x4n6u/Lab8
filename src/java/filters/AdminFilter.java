b7b5b4551ca0fb67e393bce10114722428269caa
Joshua Law <85807286+x4n6u@users.noreply.github.com>
2022-11-22 22:02:18 -0700

    Completed Lab
    Completed lab 8 task with adding in new filter

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Filter.java to edit this template
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.*;
+
+/**
+ *
+ * @author Joshs
+ */
+@WebFilter(filterName = "AdminFilter", servletNames = {"AdminServlet"})
+public class AdminFilter implements Filter {
+    
+    private static final boolean debug = true;
+
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+    
+    public AdminFilter() {
+    }    
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        
+        String role = (String) httpRequest.getSession().getAttribute("role");
+        
+        if(role != null && role.equals("admin"))
+        {
+            chain.doFilter(request, response);
+            return;
+        }
+        
+        httpResponse.sendRedirect("notes");
+        
+    }
+}
